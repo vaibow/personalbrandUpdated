670a09dd728fd0346727890b53cad045639fbcf7
vaibow <vaibhavpatil3903@gmail.com>
2024-08-01 17:24:59 +0530

    commit

---
+++ animate.js
@@ -0,0 +1,5 @@
+// animate.js
+$(document).ready(function() {
+    $("#intro-text > h2").css('opacity', 1).lettering('words').children("span").lettering().children("span").lettering();
+    $("#intro-text > h3").css('opacity', 1).lettering('words').children("span").lettering().children("span").lettering();
+});
